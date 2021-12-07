8023849931db83ff0e3e88f190cbe678cc2404c6
liming.zeng <liming.zeng@hairoutech.com>
2021-12-07 18:15:25 +0800

    feat: tcp 端口代理

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![warn(rust_2018_idioms)]
 #[macro_use]
 extern crate serde_derive;
-extern crate toml;
 
-use std::net::SocketAddr;
+use std::net::{SocketAddr};
+
 use futures::future::{join_all, try_join};
+use log::{error, info};
+use log4rs;
 use tokio;
 use tokio::fs;
 use tokio::io::{self, AsyncWriteExt};
@@ -12,59 +14,40 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     let mut runners = vec![];
-    let mut proxys = vec![];
     let file_path = "config.toml";
     let content = fs::read_to_string(file_path).await.unwrap();
     let conf: Conf = toml::from_str(&*content).unwrap();
-    let proxy = conf.proxy.unwrap();
-    println!("{:?}", proxy);
-    for c in proxy.as_slice() {
-        proxys.push( Proxy {
-            source_port: c.source_port.unwrap(),
-            target_ip: c.target_ip.unwrap(),
-            target_port: c.target_port.unwrap(),
-            listener: Option::None,
-        });
+    for c in conf.proxy {
+        runners.push(tokio::spawn(run(c.source_ip, c.source_port, c.target_ip, c.target_port)))
+    }
 
-    }
-    for proxy in proxys.iter() {
-        runners.push(tokio::spawn(async move {
-            proxy.run()
-        }));
-    }
     join_all(runners).await;
 }
 
 #[derive(Deserialize)]
 #[derive(Debug)]
 struct ProxyConfig {
-    source_port: Option<String>,
-    target_ip: Option<String>,
-    target_port: Option<String>,
+    source_ip: String,
+    source_port: String,
+    target_ip: String,
+    target_port: String,
 }
 
 #[derive(Deserialize)]
 #[derive(Debug)]
 struct Conf {
-    proxy: Option<Vec<ProxyConfig>>,
+    proxy: Vec<ProxyConfig>,
 }
 
-struct Proxy {
-    source_port: String,
-    target_ip: String,
-    target_port: String,
-    listener: Option<TcpListener>,
-}
-
-impl Proxy {
-    pub async fn run(&self) {
-        let listener = TcpListener::bind(format!("0.0.0.0:{}", self.source_port)).await.unwrap();
-        loop {
-            let (mut socket, _) = listener.accept().await.unwrap();
-            println!("receive connection");
-            transfer_tcp(socket, format!("{}:{}",self.target_ip, self.target_port).parse().unwrap()).await;
-        }
+async fn run(source_ip: String, source_port: String, target_ip: String, target_port: String) {
+    let listener = TcpListener::bind(format!("{}:{}", source_ip, source_port)).await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        info!("receive connection with source ip: {} source port: {}",source_ip, source_port);
+        tokio::spawn(transfer_tcp(socket, format!("{}:{}", target_ip, target_port).parse().unwrap()));
+        // transfer_tcp(socket, format!("{}:{}",target_ip, target_port).parse().unwrap()).await;
     }
 }
 
@@ -72,26 +55,35 @@ async fn transfer_tcp(source_socket: TcpStream, remote_add: SocketAddr) -> Resul
     source_socket.set_nodelay(true).unwrap();
     match TcpStream::connect(remote_add).await {
         Ok(target_socket) => {
-            println!("connected target");
+            info!("connected target ip: {}, target port: {}", remote_add.ip(), remote_add.port());
             target_socket.set_nodelay(true).unwrap();
             let (mut source_input, mut source_output) = io::split(source_socket);
             let (mut target_input, mut target_output) = io::split(target_socket);
-            println!("copy io");
+
             let source_to_target = async {
-                io::copy(&mut source_input, &mut target_output).await;
+                match io::copy(&mut source_input, &mut target_output).await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        error!("copy io error {}", e);
+                    }
+                };
                 target_output.shutdown().await
             };
-
             let target_to_source = async {
-                io::copy(&mut target_input, &mut source_output).await;
+                match io::copy(&mut target_input, &mut source_output).await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        error!("copy io error {}", e);
+                    }
+                };
                 source_output.shutdown().await
             };
-            try_join(source_to_target, target_to_source).await;
+            match try_join(source_to_target, target_to_source).await {
+                Ok(_) => {}
+                Err(_) => {}
+            };
         }
-        Err(e) => { println!("asdasdasd{}", e) }
+        Err(e) => { error!("connected target ip: {}, target port: {} error{}",remote_add.ip(), remote_add.port(), e) }
     }
     Ok(())
 }
-
-
-
